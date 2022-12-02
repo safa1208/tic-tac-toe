dc0cbe9cef96b8d1597bbe9aa6187f1907a5edf1
Safaahmed1208 <safaahmed1208@outlook.com>
2022-12-02 10:06:01 +0000

    python commit

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
-print('hello world')
+board = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-"]
+currentPlayer = "x"
+winner = None
+gameRunning = True
+
+# printing the game board
+def printBoard(board):
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print("------------")
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print("------------")
+    print(board[6] + " | " + board[7] + " | " + board[8])
+printBoard(board)
+# take player input
+
+# check for win or tie
+
+# switch the player
+
+# check for win or tie again
