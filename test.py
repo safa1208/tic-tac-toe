0d718a6c7fa16d235aa56b2a1f6fa811c9a371b3
Safaahmed1208 <safaahmed1208@outlook.com>
2022-12-02 12:39:40 +0000

    python commit

--- test.py
+++ test.py
@@ -14,9 +14,19 @@ def printBoard(board):
     print(board[6] + " | " + board[7] + " | " + board[8])
 printBoard(board)
 # take player input
+def playerInput(board):
+    inp = int(input("enter a number 1-9:"))
+    if inp >= 1 and inp<= 9 and board[inp-1] == "-":
+        board[inp-1] = currentPlayer
+    else:
+        print("Oops player si already in that spot!")
 
 # check for win or tie
 
 # switch the player
 
 # check for win or tie again
+
+while gameRunning:
+    printBoard(board)
+    playerInput(board)
